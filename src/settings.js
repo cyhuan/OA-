8f031516eac6e15bcfeda47f5cff02cd321c7aaa
cyhuan <huan_0131@qq.com>
2021-10-08 23:21:04 +0800

    登录页第一次提交

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,5 @@
 module.exports = {
-
-  title: 'Vue Admin Template',
+  title: "人力资源管理平台",
 
   /**
    * @type {boolean} true | false
@@ -13,4 +12,4 @@ module.exports = {
    * @description Whether show the logo in sidebar
    */
   sidebarLogo: false
-}
+};
