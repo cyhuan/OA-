4cfcca05cc73c70956e8905366d81806dc0ab3c4
cyhuan <huan_0131@qq.com>
2021-10-14 09:57:34 +0800

    员工管理第一次提交

--- src/main.js
+++ src/main.js
@@ -11,18 +11,22 @@ import '@/styles/index.scss' // global css
 import App from './App'
 import store from './store'
 import router from './router'
-
+import Component from '@/components'
 import '@/icons' // icon
 import '@/permission' // permission control
 
 import * as directvies from '@/directvies'
-
-
+import * as filters from '@/filters'
+// 注册全局的过滤器
+Object.keys(filters).forEach(key => {
+  // 注册过滤器
+  Vue.filter(key, filters[key])
+})
 // set ElementUI lang to EN
 Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
-
+Vue.use(Component) // 注册自己的插件 
 Object.keys(directvies).forEach(key => {
   Vue.directive(key, directvies[key])
 })
